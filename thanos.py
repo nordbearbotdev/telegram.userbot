92adaa1b111e4614a069aa67d37500e5d087c579
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-06-30 16:55:00 +0300

    Create thanos.py

---
+++ thanos.py
@@ -0,0 +1,31 @@
+# Команда Щелчка Таноса
+@app.on_message(filters.command("thanos", prefixes=".") & filters.me)
+def thanos(_, msg):
+    chat = msg.text.split(".thanos ", maxsplit=1)[1]
+ 
+    members = [
+        x
+        for x in app.iter_chat_members(chat)
+        if x.status not in ("administrator", "creator")
+    ]
+ 
+    random.shuffle(members)
+ 
+    app.send_message(chat, "Щелчок Таноса ... *щёлк*")
+ 
+    for i in range(len(members) // 2):
+        try:
+            app.restrict_chat_member(
+                chat_id=chat,
+                user_id=members[i].user.id,
+                permissions=ChatPermissions(),
+                until_date=int(time.time() + 86400),
+            )
+            app.send_message(chat, "Исчез " + members[i].user.first_name)
+        except FloodWait as e:
+            print("> waiting", e.x, "seconds.")
+            time.sleep(e.x)
+ 
+    app.send_message(chat, "Но какой ценой?")
+ 
+app.run()
