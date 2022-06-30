7125ffb889a0f987a629cabfbc300139e7ab5d58
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-06-30 16:53:57 +0300

    Create hack.py

---
+++ hack.py
@@ -0,0 +1,34 @@
+# Команда взлома пентагона
+@app.on_message(filters.command("hack", prefixes=".") & filters.me)
+def hack(_, msg):
+    perc = 0
+ 
+    while(perc < 100):
+        try:
+            text = "👮‍ Взлом пентагона в процессе ..." + str(perc) + "%"
+            msg.edit(text)
+ 
+            perc += random.randint(1, 3)
+            sleep(0.1)
+ 
+        except FloodWait as e:
+            sleep(e.x)
+ 
+    msg.edit("🟢 Пентагон успешно взломан!")
+    sleep(3)
+ 
+    msg.edit("👽 Поиск секретных данных об НЛО ...")
+    perc = 0
+ 
+    while(perc < 100):
+        try:
+            text = "👽 Поиск секретных данных об НЛО ..." + str(perc) + "%"
+            msg.edit(text)
+ 
+            perc += random.randint(1, 5)
+            sleep(0.15)
+ 
+        except FloodWait as e:
+            sleep(e.x)
+ 
+    msg.edit("🦖 Найдены данные о существовании динозавров на земле!")
